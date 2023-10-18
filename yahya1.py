b597f9c29dc8f92738ff6eeca93a1e50a8378b86
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-10-18 11:53:51 +0200

    Add files via upload

---
+++ yahya1.py
@@ -0,0 +1,11 @@
+
+
+y=int(input("svp dons la casse:"))
+l=[]
+for i in range(0,y):
+
+    n=input("svp done une chaîne donnée.")
+    l.append(n)
+print(l)
+x=input("la position d'une sous-chaîne dans une chaîne donnée.")
+print(l.index(x))
